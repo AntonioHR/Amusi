8f14cc7cdde9b86fc5023ea9462b4a3d04cb325e
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-03-15 16:29:22 -0300

    Added DummyFSM

---
+++ BEAT/Assets/_Test_DummyFSM/DummyMusicSelector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BeatFW;
+
+public class DummyMusicSelector : MonoBehaviour {
+    public BeatMusicController musicController;
+    public string patchToUse = "a.0";
+	// Use this for initialization
+    void Start()
+    {
+        musicController.Init(patchToUse);
+        musicController.OnClipCloseToEnd += MusicController_OnClipCloseToEnd;
+	}
+
+    private void MusicController_OnClipCloseToEnd(object sender, ClipEventArgs e)
+    {
+        musicController.QueueUpPatch(patchToUse);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
