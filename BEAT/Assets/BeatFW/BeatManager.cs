84b66b08c80e54a96c7de50cc986d8f0ff7a3238
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-03-17 16:20:04 -0300

    Added draft of BeatManager (Facade MonoBehaviour) and DummyPatchSelector

---
+++ BEAT/Assets/BeatFW/BeatManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BeatFW
+{
+    public class BeatManager:MonoBehaviour
+    {
+        public DummyPatchSelector selector;
+        
+        void Start()
+        {
+            Debug.Log("Initializing");
+        }
+    }
+}
---
+++ BEAT/Assets/BeatFW/DummyPatchSelector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BeatFW
+{
+    [CreateAssetMenu(menuName = "Beat/Tests/PatchSelector")]
+    public class DummyPatchSelector:ScriptableObject, IBeatPatchSelector
+    {
+        public AudioClip clip;
+
+        public AudioClip SelectNextPatch()
+        {
+            Debug.Assert(clip != null);
+            return clip;
+        }
+    }
+}
