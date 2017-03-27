c5878458c305145654fab1dff9efd034e4b8abb7
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-03-27 15:47:16 -0300

    Added BeatPattern

---
+++ BEAT/Assets/BeatFW/BeatPattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BeatFW
+{
+    /// <summary>
+    /// Stores the data of a Beat Pattern
+    /// </summary>
+    [System.Serializable]
+    public class BeatPattern
+    {
+        public int BeatsPerMeasure = 4;
+
+        public int Measures = 1;
+
+        public List<Note> notes;
+
+        public class Note
+        {
+            public float timeInBeats;
+        }
+    }
+}
