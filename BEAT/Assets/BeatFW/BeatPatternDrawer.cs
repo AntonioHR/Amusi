5d42b1796defac385c29bf0ba227a978a448fc5e
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-03-29 18:27:19 -0300

    Created BeatPatternDrawer Empty File

---
+++ BEAT/Assets/BeatFW/BeatPatternDrawer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace BeatFW.Editor
+{
+    [CustomPropertyDrawer(typeof(BeatPattern))]
+    public class BeatPatternDrawer : PropertyDrawer
+    {
+        void OnGUI(Rect position, SerializedProperty property, GUIContent label){
+            
+            if(GUILayout.Button("Test"))
+            {
+                Debug.Log("Yass!");
+            }
+        }
+    }
+}
