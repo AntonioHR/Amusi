3eff6e7c351a152a6a2581b61e4897c8e34e5174
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-04-03 17:51:03 -0300

    Very simple window editing

---
+++ BEAT/Assets/BeatFW/Editor/BeatPatternDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace BeatFW.Editor
+{
+    [CustomPropertyDrawer(typeof(BeatPattern))]
+    public class BeatPatternDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label){
+            var r1 = position;
+            r1.width *= .3f;
+            GUI.Label(r1, label);
+            var r2 = new Rect(position.x + r1.width, position.y, position.width - r1.width, r1.height);
+            if (GUI.Button(r2, new GUIContent("Edit")))
+            {
+                BeatPatternWindow.Show(property);
+            }
+        }
+    }
+}
---
+++ BEAT/Assets/BeatFW/Editor/BeatPatternWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace BeatFW.Editor
+{
+    public class BeatPatternWindow : EditorWindow
+    {
+        public SerializedProperty Prop { 
+            set
+            {
+                beatsPerMeasure = value.FindPropertyRelative("beatsPerMeasure");
+                measureCount = value.FindPropertyRelative("measureCount");
+                notes = value.FindPropertyRelative("notes");
+            }
+        }
+
+
+        SerializedProperty beatsPerMeasure;
+        SerializedProperty measureCount;
+        SerializedProperty notes;
+        public static void Show(SerializedProperty BeatPatternProperty)
+        {
+            EditorWindow.GetWindow<BeatPatternWindow>().Prop = BeatPatternProperty;
+        }
+        void OnGUI()
+        {
+            beatsPerMeasure.serializedObject.Update();
+            EditorGUILayout.PropertyField(beatsPerMeasure);
+            EditorGUILayout.PropertyField(measureCount);
+            EditorGUILayout.PropertyField(notes, true);
+            beatsPerMeasure.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
