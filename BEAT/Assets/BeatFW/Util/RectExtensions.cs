c635145cd40756cf1b2034d2658bd0c68854357f
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-04-19 16:54:42 -0300

    Added RectExtensions

---
+++ BEAT/Assets/BeatFW/Util/RectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BeatFW.Util
+{
+    public static class RectExtensions
+    {
+
+        public static Rect MinusMargin(this Rect r, Vector2 margins)
+        {
+            return new Rect(r.position + margins / 2, r.size - margins);
+        }
+        public static Rect AtOrigin(this Rect r)
+        {
+            return new Rect(Vector2.zero, r.size);
+        }
+    }
+}
