c3f46b6ca77542ea9f7f251e2277f335a5176cc9
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-04-26 15:16:36 -0300

    Added Scroll View

--- BEAT/Assets/_EditorTests/Editor/StyleTestsWindow.cs
+++ BEAT/Assets/_EditorTests/Editor/StyleTestsWindow.cs
@@ -22,7 +22,7 @@ public class StyleTestWindow : EditorWindow {
         skin = Resources.Load<GUISkin>("TestSkin");
         toggles = skin.button;
     }
-
+    float dummy;
     void OnGUI()
     {
         //Vector2 margin = 20 * Vector2.one;
@@ -31,14 +31,26 @@ public class StyleTestWindow : EditorWindow {
         EditorGUI.DrawRect(inside, Color.gray);
 
         arr = (SimpleArray)EditorGUI.ObjectField(new Rect(inside.position - Vector2.up * 2 * EditorGUIUtility.singleLineHeight, new Vector2(100, EditorGUIUtility.singleLineHeight)), arr, typeof(SimpleArray));
-        arr.offset = EditorGUI.FloatField(new Rect(inside.position - Vector2.up * EditorGUIUtility.singleLineHeight, new Vector2(100, EditorGUIUtility.singleLineHeight)), "Offset", arr.offset);
 
-        int length = arr.data.Length;
-        var grid = inside.GetMaskedHorizontalGridInside(arr.offset, length, 30, 10, Vector2.one * 10);
-        for (int i = 0; i < length; i++)
+        if (arr != null)
         {
-            if(grid.isValid[i])
-                arr.data[i] = GUI.Toggle(grid.Slots[i], arr.data[i], GUIContent.none, toggles);
+            int length = arr.data.Length;
+            float spacing = 10;
+            float slotWidth = 30;
+            var gridArea = inside.MinusMargin(0, EditorGUIUtility.singleLineHeight, 0, 0);
+
+            Vector2 margin = Vector2.one * 10;
+            float viewWidth = gridArea.width;
+            float totalWidth = (spacing * (length - 1)) + slotWidth * length + margin.x;
+            totalWidth = totalWidth > viewWidth ? totalWidth : viewWidth;
+            arr.offset = GUI.HorizontalScrollbar(new Rect(inside.position.x, inside.position.y, inside.width, EditorGUIUtility.singleLineHeight), arr.offset, viewWidth, 0, totalWidth);
+
+            var grid = gridArea.GetMaskedHorizontalGridInside(arr.offset, length, slotWidth, spacing, margin);
+            for (int i = 0; i < length; i++)
+            {
+                if (grid.isValid[i])
+                    arr.data[i] = GUI.Toggle(grid.Slots[i], arr.data[i], GUIContent.none, toggles);
+            }
         }
     }
 }
