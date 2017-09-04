81ca51fe28a333499320be067abc79da48d15737
Antonio Harger Rivero <antonio.rivero@gprt.ufpe.br>
2017-09-04 17:27:04 -0300

    Basic Music Tree Data

---
+++ BEAT/Assets/AntonioHR/MusicTree/MusicTree.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AntonioHR.TreeAsset;
+
+namespace AntonioHR.MusicTree
+{
+    public class MusicTree : TreeAsset<MusicTreeNode>
+    {
+
+        protected override MusicTreeNode InstantiateRoot()
+        {
+            var result = ScriptableObject.CreateInstance<SelectorMusicTreeNode>();
+            result.name = "Root(Selector)";
+            return result;
+        }
+
+        protected override MusicTreeNode InstantiateDefaultNode()
+        {
+            var result = ScriptableObject.CreateInstance<SelectorMusicTreeNode>();
+            result.name = "Node(Selector)";
+            return result;
+        }
+    }
+}
---
+++ BEAT/Assets/AntonioHR/MusicTree/MusicTreeNode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AntonioHR.TreeAsset;
+
+namespace AntonioHR.MusicTree
+{
+    public abstract class MusicTreeNode : TreeNodeAsset
+    {
+        public enum ChildrenPolicy { None, Single, Multiple}
+        public abstract ChildrenPolicy Policy { get; }
+    }
+    public class SelectorMusicTreeNode : MusicTreeNode
+    {
+        public override ChildrenPolicy Policy
+        {
+            get { return ChildrenPolicy.Multiple; }
+        }
+    }
+    public class SequenceMusicTreeNode : MusicTreeNode
+    {
+        public override ChildrenPolicy Policy
+        {
+            get { return ChildrenPolicy.Multiple; }
+        }
+    }
+    public class CueMusicTreeNode : MusicTreeNode
+    {
+        public override ChildrenPolicy Policy
+        {
+            get { return ChildrenPolicy.None; }
+        }
+        public AudioClip clip;
+    }
+}
