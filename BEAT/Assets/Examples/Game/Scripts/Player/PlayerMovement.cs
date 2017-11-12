d3542be9c22e75aac2da78295f63a30ce3c8cc21
Antonio Harger Rivero <antonio.harger@gmail.com>
2017-11-12 15:47:48 -0200

    Cross and X Patterns

--- BEAT/Assets/Examples/Game/Scripts/Player/PlayerMovement.cs
+++ BEAT/Assets/Examples/Game/Scripts/Player/PlayerMovement.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+
+    
     [Serializable]
     public class Settings
     {
@@ -22,7 +24,10 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	void Update () {
+
         var move = new Vector3(HorizontalInput, 0, VerticalInput);
+        float y = Camera.main.transform.rotation.eulerAngles.y;
+        //move = Quaternion.AngleAxis(y, Vector3.up) * move;
         body.velocity = move * settings.speed; ;
         
 	}
---
+++ BEAT/Assets/PlayerCrossXShooter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AntonioHR.Amusi;
+using System;
+
+public class PlayerCrossXShooter :  MultiMonoDancer
+{
+    [Serializable]
+    public class Pattern
+    {
+        public Vector3[] directions;
+    }
+    public GameObject bulletPrefab;
+
+
+    int index;
+
+
+    public Pattern[] patterns;
+
+    protected override void Init()
+    {
+    }
+
+    protected override void OnNoteEnd(int i)
+    {
+    }
+
+    protected override void OnNoteStart(int i)
+    {
+        foreach (var dir in patterns[index].directions)
+        {
+            var shootDir = dir.normalized;
+            var bullet = Instantiate(bulletPrefab).GetComponent<PlayerBullet>();
+            bullet.transform.position = transform.position;
+            bullet.transform.rotation = Quaternion.FromToRotation(Vector3.up, shootDir);
+            bullet.Direction = shootDir;
+        }
+
+        index++;
+        index = index % patterns.Length;
+    }
+
+    protected override void OnNoteUpdate(int i, float progress)
+    {
+    }
+
+}
